7b1823a9065a0e6364aca1c112a25c19cc053bde
Koviko <volectricity@gmail.com>
2020-09-22 17:53:10 -0400

    First live attempt

--- src/js/main.js
+++ src/js/main.js
@@ -12,29 +12,38 @@ import LazyLoad from "vanilla-lazyload";
   *******************************/
 
   let searchTrigger = document.getElementById('searchTrigger');
+  let mobileSearchTrigger = document.getElementById('mobileSearchTrigger');
   let searchInputWrapper = document.getElementById('searchInputWrapper');
+  let mobileSearchInputWrapper = document.getElementById('mobileSearchInputWrapper');
   let menu = document.getElementById('navWrapper');
 
 
   searchTrigger.addEventListener("click", openSearch);
+  mobileSearchTrigger.addEventListener("click", openSearch);
   document.addEventListener("click", closeSearch);
   // Call Function On Click
   function openSearch() {
     searchInputWrapper.classList.toggle('active');
+    mobileSearchInputWrapper.classList.toggle('active');
     searchTrigger.classList.toggle('active');
+    mobileSearchTrigger.classList.toggle('active');
     menu.classList.toggle('active');
   }
 
   function closeSearch() {
-    let isClickInsideElement = searchInputWrapper.contains(event.target);
-    let isClickInsideSearchTrigger = searchTrigger.contains(event.target);
+    let isClickInsideElement = searchInputWrapper.contains(event.target) || mobileSearchInputWrapper.contains(event.target);
+    let isClickInsideSearchTrigger = searchTrigger.contains(event.target) || mobileSearchTrigger.contains(event.target);
     if (isClickInsideSearchTrigger) {
       searchInputWrapper.classList.add('active');
+      mobileSearchInputWrapper.classList.add('active');
       searchTrigger.classList.add('active');
+      mobileSearchTrigger.classList.add('active');
       menu.classList.add('active')
     } else if (!isClickInsideElement && !isClickInsideSearchTrigger) {
       searchInputWrapper.classList.remove('active');
+      mobileSearchInputWrapper.classList.remove('active');
       searchTrigger.classList.remove('active');
+      mobileSearchTrigger.classList.remove('active');
       menu.classList.remove('active');
     }
   }
@@ -63,13 +72,18 @@ import LazyLoad from "vanilla-lazyload";
    Mobile Menu Dropdown
   ******************/
   let mobileTrigger = document.getElementById('mobileTrigger');
+  let secondaryMobileTrigger = document.getElementById('secondaryMobileTrigger');
   let mobileDropdown = document.getElementById('mobileDropdown');
+  let secondaryMobileDropdown = document.getElementById('secondaryMobileDropdown');
 
   mobileTrigger.addEventListener("click", toggleMobileMenu);
+  secondaryMobileTrigger.addEventListener("click", toggleMobileMenu);
 
   function toggleMobileMenu() {
     mobileDropdown.classList.toggle('open');
+    secondaryMobileDropdown.classList.toggle('open');
     mobileTrigger.classList.toggle('open');
+    secondaryMobileTrigger.classList.toggle('open');
     document.body.classList.toggle('open-menu');
   }
 
