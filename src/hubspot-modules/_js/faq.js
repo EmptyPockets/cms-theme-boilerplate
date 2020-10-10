5150230446043f6242835f8cc4a7cd13ed0b2318
Koviko <volectricity@gmail.com>
2020-10-10 14:07:48 -0400

    Removed requirement for all pages to have a FAQs section

--- src/hubspot-modules/_js/faq.js
+++ src/hubspot-modules/_js/faq.js
@@ -2,42 +2,35 @@
 
   const faqs = document.querySelectorAll('.faq .full-answer');
   const faqWrapper = document.getElementById('faqWrapper');
-  faqWrapper.addEventListener('click', toggleQuestion, false);
 
-  function init() {
-    /* Close all but the 'always-open' faqs */
-    for (const faq of faqs) {
-      if (!faq.classList.contains('starts-open')) {
-        faq.classList.remove('open');
-      }
+  if (faqs && faqWrapper) {
+    faqWrapper.addEventListener('click', toggleQuestion, false);
 
-    }
-  }
+    function init() {
+      /* Close all but the 'always-open' faqs */
+      for (const faq of faqs) {
+        if (!faq.classList.contains('starts-open')) {
+          faq.classList.remove('open');
+        }
 
-  function toggleQuestion(e) {
-    const faq = e.target.closest('.faq');
-    if (faq) {
-      faq.classList.toggle('open');
-      const question = faq.querySelector('.full-answer');
-      const height = question.scrollHeight;
-      question.classList.toggle('open');
-      if (question.classList.contains('open')) {
-        question.style.maxHeight = height + 'px';
-      } else {
-        question.style.maxHeight = 0 + 'px';
       }
     }
+
+    function toggleQuestion(e) {
+      const faq = e.target.closest('.faq');
+      if (faq) {
+        faq.classList.toggle('open');
+        const question = faq.querySelector('.full-answer');
+        const height = question.scrollHeight;
+        question.classList.toggle('open');
+        if (question.classList.contains('open')) {
+          question.style.maxHeight = height + 'px';
+        } else {
+          question.style.maxHeight = 0 + 'px';
+        }
+      }
+    }
+
+    init();
   }
-
-
-
-
-
-
-
-  init();
-
-
-
 })();
-
